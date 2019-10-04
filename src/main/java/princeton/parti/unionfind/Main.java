fc52b0e0f03d1a2505ff4bb03f70f7890f540167
Rybakova Anastasia <arybakova@marathonbet.ru>
2019-10-04 16:19:53 +0300

    Princeton Algorithms Part I Union Find

---
+++ src/main/java/princeton/parti/unionfind/Main.java
@@ -0,0 +1,32 @@
+package princeton.parti.unionfind;
+
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+
+        final int N = scanner.nextInt();
+
+        final QuickFindUF uf = new QuickFindUF(N);
+
+        while (scanner.hasNext())
+        {
+            final int p = scanner.nextInt();
+
+            final int q = scanner.nextInt();
+
+            if(!uf.connected(p,q))
+            {
+                uf.union(p, q);
+
+                System.out.println(p + " " + q);
+            }
+        }
+    }
+}
---
+++ src/main/java/princeton/parti/unionfind/WightedQuickUnionUF.java
@@ -0,0 +1,47 @@
+package princeton.parti.unionfind;
+
+
+public class WightedQuickUnionUF
+{
+    private int[] id;
+
+    private int[] size;
+
+
+    public WightedQuickUnionUF(int N)
+    {
+        id = new int[N];
+        size = new int[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            id[i] = i;
+            size[i] = 1;
+        }
+    }
+
+
+    private int root(int i)
+    {
+        while (i != id[i])
+        {
+            i = id[i];
+        }
+
+        return i;
+    }
+
+
+    public boolean connected(int p, int q)
+    {
+        return root(p) == root(q);
+    }
+
+
+    public void union(int p, int q)
+    {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+}
